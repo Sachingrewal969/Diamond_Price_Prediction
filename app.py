09f7deaea9f0877c74e82f4344a77e97d467d49c
Sachingrewal969 <sachingrewal969@gmail.com>
2024-10-16 20:02:46 +0530

    u

--- app.py
+++ app.py
@@ -40,4 +40,7 @@ def predict_datapoint():
         
         return render_template("result.html",final_result=result)
 
+# #execution begin
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
 
